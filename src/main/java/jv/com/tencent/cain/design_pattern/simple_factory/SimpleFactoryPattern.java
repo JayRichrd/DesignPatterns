e988367f429c74a652c4bfededaac754bfe5a110
cainjiang <cainjiang@tencent.com>
2019-02-20 17:35:23 +0800

    feat(init): 初始化项目

---
+++ src/main/java/jv/com/tencent/cain/design_pattern/simple_factory/SimpleFactoryPattern.java
@@ -0,0 +1,12 @@
+package jv.com.tencent.cain.design_pattern.simple_factory;
+
+/**
+ * @author cainjiang
+ * @date 2019-02-18
+ * 简单工厂设计模式
+ */
+public class SimpleFactoryPattern {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/kotlin/kt/com/tencent/cain/desigin_pattern/simple_factoty/SimpleFactoryPattern.kt
@@ -0,0 +1,10 @@
+package kt.com.tencent.cain.desigin_pattern.simple_factoty
+
+/**
+ * @author cainjiang
+ * @date 2019-02-18
+ * 简单设计模式
+ */
+fun main(args: Array<String>) {
+    println("haha")
+}
